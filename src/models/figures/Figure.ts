c90d11f659e3212047648153d512cb3d866fdbde
Andrei Medvedev <60568916+AndreiMedvedevSaratov@users.noreply.github.com>
2022-07-31 16:40:49 +0400

    feat: created figures

--- src/models/figures/Figure.ts
+++ src/models/figures/Figure.ts
@@ -1,3 +1,38 @@
+import { Colors } from "../Colors";
+import logo from '../../assets/black-king.png';
+import { Cell } from "../Cell";
+
+export enum FigureNames {
+	FIGURE = "Фигура",
+	KING = "Король",
+	QUEEN = "Ферзь",
+	BISHOP = "Слон",
+	KNIGHT = "Конь",
+	ROOK = "Ладья",
+	PAWN = "Пешка",
+}
+
 export class Figure {
-	
+	color: Colors;
+	logo: typeof logo | null;
+	cell: Cell;
+	name: FigureNames;
+	id: number;
+
+	constructor(color: Colors, cell: Cell) {
+		this.color = color;
+		this.cell = cell;
+		this.cell.figure = this;
+		this.logo = null;
+		this.name = FigureNames.FIGURE
+		this.id = Math.random()
+	}
+
+	cnaMove(target: Cell): boolean {
+		return true;
+	}
+
+	moveFigure(target: Cell) {
+
+	}
 }
